c443523d3946cc534e3cb4df8cf19815543b6c25
unknown <anwarhosen575@gmail.com>
2021-10-18 15:49:38 +0600

    firebase config

---
+++ src/Pages/Login/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD-VNe9K7hZpLJxe7Fu3OsSSANDzRVGH4U",
+    authDomain: "genius-car-mechanics-5a42e.firebaseapp.com",
+    projectId: "genius-car-mechanics-5a42e",
+    storageBucket: "genius-car-mechanics-5a42e.appspot.com",
+    messagingSenderId: "767133268496",
+    appId: "1:767133268496:web:c960d1c17e73a29d536895"
+};
+
+export default firebaseConfig;
