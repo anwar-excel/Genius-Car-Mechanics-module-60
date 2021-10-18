c14023964a9eff6926e239fd33ad6e061e6974c9
unknown <anwarhosen575@gmail.com>
2021-10-18 12:21:19 +0600

    header

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Home from './Pages/Home/Home/Home';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import NotFound from './Pages/Home/NotFound/NotFound';
 import Booking from './Pages/Booking/Booking/Booking';
+import Header from './Pages/Shared/Header/Header';
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Header></Header>
         <Switch>
           <Route exact path="/">
             <Home></Home>
