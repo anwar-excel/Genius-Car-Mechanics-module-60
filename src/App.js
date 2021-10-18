3a30a60c7d0af439439b521e94d1259b2a3972d5
unknown <anwarhosen575@gmail.com>
2021-10-18 11:03:24 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
 import Home from './Pages/Home/Home/Home';
-
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import NotFound from './Pages/Home/NotFound/NotFound';
 
 function App() {
   return (
     <div className="App">
-      <Home></Home>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+          <Route path="/home">
+            <Home></Home>
+          </Route>
+          <Route path="*">
+            <NotFound></NotFound>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -5,12 +5,13 @@ const Service = ({ service }) => {
     // const {service} = props;
     const { name, price, description, img } = service;
     return (
-        <div className="service">
+        <div className="service pb-3">
             <img src={img} alt="" />
             <h3>{name}</h3>
             <h5>Price: {price}</h5>
             <p className="px-3">{description}</p>
-        </div>
+            <button className="btn btn-warning">Book {name.toLowerCase()}</button>
+        </div >
     );
 };
 
